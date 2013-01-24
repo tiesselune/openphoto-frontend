0dc2d93feff6cc32c23b82b03edb8c0e3f6ea555
tiesselune <julien_sosthene@yahoo.co.uk>
2013-01-24 17:39:05 +0100

    update upload hook. Works fine. TODO: delete

---
+++ src/scripts/open-photo-hook/credentials.php
@@ -0,0 +1,8 @@
+<?php
+$repo = "../";
+$host="local.photo";
+$consumerKey="4167bb25e2be423ce12625bd4214a0";
+$consumerSecret="a67413867c";
+$token="7caa5bfa923ea1bf94553831cfa47f";
+$tokenSecret="265458679a";
+?>
