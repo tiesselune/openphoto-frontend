ffe565f0e6db4dac1f31f67565d38d8513259fda
tiesselune <julien_sosthene@yahoo.co.uk>
2013-01-24 03:35:59 +0100

    Made a partial hook. Still buggy, misses delete and update actions.

---
+++ src/scripts/credentials.php
@@ -0,0 +1,8 @@
+<?php
+$repo = "/var/www/local.photo/open-photo/src/html/photos/original";
+$host="local.photo";
+$consumerKey="4167bb25e2be423ce12625bd4214a0";
+$consumerSecret="a67413867c";
+$token="7caa5bfa923ea1bf94553831cfa47f";
+$tokenSecret="265458679a";
+?>
